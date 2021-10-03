7b1419e940d19b0425e07bbd8972b88c43790a80
iamstephan <stephanburger54@gmail.com>
2021-10-03 12:34:09 +0200

    click-ouside and scroll handlers for closing the nav

--- src/scripts/components/supernav.ts
+++ src/scripts/components/supernav.ts
@@ -12,10 +12,17 @@
 class MobileNav {
   supernavs: NodeListOf<HTMLDivElement>;
 
+  previousTop: number = window.scrollY;
+  currentTop: number = 0;
+  viewportWidth: number = window.innerWidth;
+  scrollThreshold: number = 25;
+
   constructor() {
     this._getSupernavs();
-
     this._configureSuperNav();
+
+    this._handleScroll();
+    this._handleClickOutside();
   }
 
   private _getSupernavs() {
@@ -81,6 +88,63 @@ class MobileNav {
 
     submenus.forEach((submenu) => {
       submenu.querySelector(".level-2").classList.remove("show");
+    });
+  }
+
+  private _hideOnScroll() {
+    this.currentTop = window.scrollY;
+
+    const isFastEnough =
+      Math.abs(this.previousTop - this.currentTop) > this.scrollThreshold;
+
+    if (isFastEnough) {
+      this.supernavs.forEach((supernav) => {
+        const rootNode =
+          supernav.querySelector<HTMLUListElement>(".level-1.small");
+        const subMenus =
+          supernav.querySelectorAll<HTMLLIElement>(".with-children");
+
+        if (rootNode.classList.contains("show")) {
+          this._resetAll(rootNode, subMenus);
+        }
+        rootNode.classList.remove("show");
+      });
+    }
+
+    this.previousTop = this.currentTop;
+  }
+
+  private _handleScroll() {
+    let handler = () =>
+      !window.requestAnimationFrame
+        ? setTimeout(this._hideOnScroll.bind(this), 250)
+        : requestAnimationFrame(this._hideOnScroll.bind(this));
+
+    document.addEventListener("scroll", handler, {
+      passive: true,
+    });
+  }
+
+  private _handleClickOutside() {
+    this.supernavs.forEach((supernav) => {
+      const toggleButton =
+        supernav.querySelector<HTMLButtonElement>(".menu-button");
+      const rootNode =
+        supernav.querySelector<HTMLUListElement>(".level-1.small");
+      const subMenus =
+        supernav.querySelectorAll<HTMLLIElement>(".with-children");
+
+      document.addEventListener("click", (e) => {
+        const withinboundries = e.composedPath().includes(rootNode);
+        const toggleInPath = e.composedPath().includes(toggleButton);
+
+        if (!withinboundries && !toggleInPath) {
+          if (rootNode.classList.contains("show")) {
+            this._resetAll(rootNode, subMenus);
+          }
+          rootNode.classList.remove("show");
+        }
+      });
     });
   }
 }
